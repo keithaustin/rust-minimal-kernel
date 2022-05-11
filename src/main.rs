348d41dc56f6a85faf20a1902e9af06007f5e1c0
Keith Austin <keithaustin@kdakeys.com>
2022-05-11 17:08:42 -0400

    Implement VGA buffer

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+// Exclude standard library and remove Rust-level entry points
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+// Handle panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
+}
+
+// Entry point for binary
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello world{}", "!");
+
+    loop{}
+}
+
+//#![feature(custom_test_frameworks)]
+//#![test_runner(crate::test_runner)]
+
+//#[cfg(test)]
+//fn test_runner(tests: )
